b396a0e43c5af3368bd91efd6e28040e404068b3
flackner <fabian@localhost.localdomain>
2023-09-29 13:43:33 +0200

    update dotfiles

---
+++ doStow.sh
@@ -0,0 +1,19 @@
+stow --adopt git
+cp -r git git_bak
+git restore git
+
+stow --adopt tmux
+cp -r tmux tmux_bak
+git restore tmux
+
+stow --adopt zsh
+cp -r zsh zsh_bak
+git restore zsh
+
+stow --adopt vscode
+cp -r vscode vscode_bak
+git restore vscode
+
+stow --adopt mc
+cp -r mc mc_bak
+git restore mc
